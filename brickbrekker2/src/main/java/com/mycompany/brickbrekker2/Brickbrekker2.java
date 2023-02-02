3e8c44b1e1e48586fc86e61766093e8ab131e259
shantanupujari <124152996+shantanupujari@users.noreply.github.com>
2023-02-02 10:03:37 +0530

    Add files via upload

---
+++ brickbrekker2/src/main/java/com/mycompany/brickbrekker2/Brickbrekker2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.brickbrekker2;
+
+/**
+ *
+ * @author user
+ */
+public class Brickbrekker2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
